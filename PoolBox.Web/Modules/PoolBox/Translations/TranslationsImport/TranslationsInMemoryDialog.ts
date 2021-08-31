83d9dad917a6d75693147e4de18ce8f76b2799dd
Dominik Chynoradsky <dominik.chynoradsky@tembis.ch>
2021-08-31 14:45:55 +0200

    17-translations-import-in-memory-row-update

---
+++ PoolBox.Web/Modules/PoolBox/Translations/TranslationsImport/TranslationsInMemoryDialog.ts
@@ -0,0 +1,37 @@
+﻿namespace PoolBox.PoolBox {
+
+    @Serenity.Decorators.registerClass()
+    export class TranslationsInMemoryDialog extends Common.GridEditorDialog<TranslationsRow> {
+        protected getFormKey() { return TranslationsForm.formKey; }
+        protected getIdProperty() { return TranslationsRow.idProperty; }
+        protected getLocalTextPrefix() { return TranslationsRow.localTextPrefix; }
+        protected getNameProperty() { return TranslationsRow.nameProperty; }
+        protected getService() { return TranslationsService.baseUrl; }
+        protected getDeletePermission() { return TranslationsRow.deletePermission; }
+        protected getInsertPermission() { return TranslationsRow.insertPermission; }
+        protected getUpdatePermission() { return TranslationsRow.updatePermission; }
+
+        protected form = new TranslationsForm(this.idPrefix);
+
+        constructor() {
+            super();
+
+        }
+
+        protected saveHandler(options: Serenity.ServiceOptions<Serenity.SaveResponse>,
+            callback: (response: Serenity.SaveResponse) => void) {
+
+            var entity: TranslationsRow = options.request.Entity;
+
+            if (this.form.Original.value
+                && this.form.Translated.value
+                && this.form.WordType.value
+            ) {
+                entity.Original = this.form.Original.value;
+                entity.Translated = this.form.Translated.value;
+                entity.WordType = this.form.WordType.value;
+            }
+            super.saveHandler(options, callback);
+        }
+    }
+}
